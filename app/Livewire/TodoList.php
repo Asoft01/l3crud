504fab3248f173daa7f3baf4cd33f6e225bc1c6b
Adeleke Hammed Lekan <lekhad19@gmail.com>
2023-10-04 09:56:00 +0100

    Pagination

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,18 +2,39 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+    #[Rule('required|min:3|max:50')]
+    
     public $name; 
-
+    public $search;
     public function create(){
         // dd('test');
-        
+        // validate 
+        // create the todo 
+        // clear the input 
+        // send flash message 
+
+        // $this->validate();
+        $validated = $this->validateOnly('name'); 
+
+        Todo::create($validated);
+
+        $this->reset('name');
+
+        session()->flash('success', 'created');
     }
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', [
+            // 'todos' => Todo::latest()->get()
+            'todos' => Todo::latest()->paginate(5)
+        ]);
     }
 }
